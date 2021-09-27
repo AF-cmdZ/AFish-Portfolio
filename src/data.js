ea2534013cb61013b3c2467b0f5d06df20c6aff1
A Fish <ashley.paz.fish@gmail.com>
2021-09-27 16:21:38 -0500

    projects data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "My Daily",
+      subtitle: "JS and fetch API",
+      description:
+        "Time-saving landing page with multiple applications",
+      image: "../public/images/myDaily.png",
+      link: "https://af-cmdz.github.io/myDaily/",
+    },
+    {
+      title: "JavaScript Quiz",
+      subtitle: "JS, HTML, CSS",
+      description:
+        "Interactive quiz with score-keeping ability",
+      image: "../public/images/javaScript_Quiz.png",
+      link: "https://af-cmdz.github.io/JavaScript_Quiz/",
+    },
+    {
+      title: "The Iron Throne",
+      subtitle: "MVC, JS, Handlebars, mySQL",
+      description:
+        "A Game of Thrones themed Personality Quiz created by myself and team of developers from bootcamp",
+      image: "../public/images/theIronThrone.png",
+      link: "https://stormy-bastion-03201.herokuapp.com/",
+    },
+    {
+      title: "Project 3",
+      subtitle: "MERN, JS, React, Mongoose",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://source.unsplash.com/random",
+      link: "#",
+    },
+  ];
